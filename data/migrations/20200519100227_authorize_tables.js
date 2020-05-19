4ed38678e74c711f85ed380140cc990052d09415
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-19 13:10:22 -0600

    set up profiles table in migrate file

--- data/migrations/20200519100227_authorize_tables.js
+++ data/migrations/20200519100227_authorize_tables.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('profiles',prof=>{
+      prof.increments();
+      prof.string('username').notNullable().unique();
+      prof.string('passowrd').notNullable();
+      prof.string('department').notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('profiles');
 };
